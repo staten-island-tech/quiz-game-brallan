fb795bc6e7246e83e026d8a8f3bdced70462b9a5
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-04-07 10:00:06 -0400

    Experimenting w/ js and npm

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 console.log("If you see this you're good to go");
+
+const start = document.querySelector('#button start');
+start.addEventListener('click', function(e){
+    console.log("Let's get started!");
+})
