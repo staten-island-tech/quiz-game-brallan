d8c8c7c5709da2e6a5fbb084aa9ac5d97dce8d78
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-04-09 18:55:15 -0400

    I stole random code to test querySelector
    It did not go well

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
 console.log("If you see this you're good to go");
 
-const start = document.querySelector('#button start');
-start.addEventListener('click', function(e){
-    console.log("Let's get started!");
-})
+domselectors = {
+
+    displaycontainer: document.querySelector(".book-list"),
+}
+
+console.log(domselectors.displaycontainer);
+
+const listener = domselectors.displaycontainer.addEventListener("click", function(){});
+// const start = document.querySelector('#button start');
+// start.addEventListener('click', function(e){
+//     console.log("Let's get started!");
+// });
+
+//  const list = document.querySelector('#book-list ul');
+//  list.addEventListener('click', function(e){
+//     console.log("is this thing on?"); });  
