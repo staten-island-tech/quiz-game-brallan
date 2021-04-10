2c7309923880fd71b6829b97989664a975585855
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-04-09 20:02:24 -0400

    Things are working now
    yay

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,27 @@ console.log("If you see this you're good to go");
 
 domselectors = {
 
-    displaycontainer: document.querySelector(".book-list"),
+    displaycontainer: document.querySelector(".start"),
 }
 
 console.log(domselectors.displaycontainer);
 
-const listener = domselectors.displaycontainer.addEventListener("click", function(){});
+const listener = domselectors.displaycontainer.addEventListener("click", function(e){
+     console.log("YES FINALLY")
+});
+
+// const scorekeeper = function(score){
+
+// }
+
+
+
 // const start = document.querySelector('#button start');
 // start.addEventListener('click', function(e){
 //     console.log("Let's get started!");
 // });
 
-//  const list = document.querySelector('#book-list ul');
+ //const list = document.querySelector('#book-list ul .name');
 //  list.addEventListener('click', function(e){
 //     console.log("is this thing on?"); });  
+//console.log(list);
