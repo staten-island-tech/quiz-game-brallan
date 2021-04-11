3ba34c0d6a59f7f4e704e9a7558486ddcf07efe6
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-04-10 23:41:15 -0400

    A very good start
    Figured out the buttons and stuff for good!

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,33 @@
 console.log("If you see this you're good to go");
 
-domselectors = {
+//console.log(domselectors.displaycontainer);
+const getquiz = document.querySelector("#quiz-content");
+const intropage = document.querySelector('.intro-page');
+const startbtn = document.querySelector(".start");
 
-    displaycontainer: document.querySelector(".start"),
+
+
+const listener = startbtn.addEventListener("click", function(e){
+     console.log("YES FINALLY")
+     startquiz()
+});
+
+const startquiz = function(){
+    intropage.classList.add('hidden'); //This hides the intro page 
+    getquiz.classList.remove('hidden'); //This makes the hidden question appear
+    nextquestion(); //Sets up listener for submit button
 }
 
-console.log(domselectors.displaycontainer);
+const enterquestions = function(e){
 
-const listener = domselectors.displaycontainer.addEventListener("click", function(e){
-     console.log("YES FINALLY")
-});
+} //Will change question(s) to the actual question
+
+const nextquestion = function(){
+    const submit = document.querySelector('.submit');
+    const clicksubmit = submit.addEventListener('click', thing=>{
+        console.log("Next. Question.");
+    }) //Listens for clicking submit button
+}
 
 // const scorekeeper = function(score){
 
@@ -17,7 +35,7 @@ const listener = domselectors.displaycontainer.addEventListener("click", functio
 
 
 
-// const start = document.querySelector('#button start');
+//const start = document.querySelector('#button start');
 // start.addEventListener('click', function(e){
 //     console.log("Let's get started!");
 // });
