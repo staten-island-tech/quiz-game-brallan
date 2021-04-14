93569477f703b8aea2fc3daf758683b0d57c170e
B-Chappy <71845545+B-Chappy@users.noreply.github.com>
2021-04-13 21:59:06 -0400

    More js functions

--- src/js/index.js
+++ src/js/index.js
@@ -15,48 +15,47 @@ const listener = startbtn.addEventListener("click", function(e){
 const startquiz = function(){
     intropage.classList.add('hidden'); //This hides the intro page 
     getquiz.classList.remove('hidden'); //This makes the hidden question appear
-    enterquestions();
+    enterquestions(0);
     nextquestion(); //Sets up listener for submit button
 }
 
-const enterquestions = function(){
+const enterquestions = function(x){
     const qgrab = document.querySelector('#question')
     
     
         
     
-    qgrab.innerHTML = `<div> ${database[0]} </div>`
+    qgrab.innerHTML = `<div> ${database[x]} </div>`
 } //Will change question(s) to the actual question
 
 const nextquestion = function(){
     const submit = document.querySelector('.submit');
+    let x = 0;
     const clicksubmit = submit.addEventListener('click', thing=>{
+        // for(x = 0; x <5; x++){
+        //    // console.log(`Question ${x}`);
+        // }
         
-        console.log("Next. Question.");
-    }) //Listens for clicking submit button
+        ++x;
+        console.log(x);
+        enterquestions(x);
+        
+
+        
+       });   //Listens for clicking submit button
     
 }
 
 const database = [
-    "0 What color is my underwear?", "1 this is bchappy", "2 bye"
+    "0 What color is my underwear?", "1 this is bchappy", "2 bye", "3 yes!"
 ]
-console.log(database[2]);
+
 
 const answers = [
     "White", "Green", "Red",
 ]
 
-/*const checkanswer = function(){
-    correct = answers[0];
-    
-    const selectans = document.querySelectorAll('#choices');
- selectans.forEach(function(btn){
-     btn.addEventListener('click',function(e){
-         console.log("you did it");
- },);
 
- }
- ,)};    */
 // const scorekeeper = function(score){
 
 // }
@@ -66,4 +65,4 @@ const answers = [
 //const start = document.querySelector('#button start');
 // start.addEventListener('click', function(e){
 //     console.log("Let's get started!");
-// });
+//
